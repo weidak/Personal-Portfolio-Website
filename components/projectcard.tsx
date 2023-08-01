b38ba0f72d84cd2e6233fc84f8e88612fccbcdae
Tay Weida <weida.tay99@gmail.com>
2023-08-02 02:47:39 +0800

    Minor aesthetic changes

--- components/projectcard.tsx
+++ components/projectcard.tsx
@@ -1,12 +1,14 @@
-import { Card, Stack, CardBody, Text, Image, Icon, Link, CardFooter, Flex, Button, Badge} from "@chakra-ui/react";
+import { Text, Image, Icon, Link, Flex, Button, Stack} from "@chakra-ui/react";
 import { IconType } from "react-icons";
-import { MdOutlineSchool, MdPersonOutline, MdLink, MdOutlineDoneAll } from "react-icons/md";
+import { MdLink } from "react-icons/md";
+import IconContainer from "./iconcontainer";
+import { useState } from "react";
 
 interface ProjectCardProps {
   theme: any,
   title: string,
   description: string,
-  techStack: IconType[],
+  techStack: string[],
   image: string,
   url: string,
 }
@@ -19,6 +21,22 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ theme, title, description, te
     const titleColor = "gray.900"
     const descriptionColor = "gray.800"
 
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+  
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
+
+    const cardStyle: React.CSSProperties = {
+      transition: 'transform 0.3s ease',
+      cursor: '',
+      transform: isHovered ? 'scale(1.05)' : 'scale(1)',
+    }
+
     return (
       // Outer Card
       <Flex 
@@ -26,12 +44,15 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ theme, title, description, te
       bg={backgroundColor}
       p="5px"
       h="395px"
-      w={{ base: "300px", md:"345px"}}
+      w={{ base: "300px" }}
       boxShadow="dark-lg"
-      transform="transform 0.3s ease"
-      _hover={{
-        transform: "scale(1.02)",
-      }}
+      // transform="transform 0.3s ease"
+      // _hover={{
+      //   transform: "scale(1.02)",
+      // }}
+      style={cardStyle}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       >
         {/* Inner Card */}
         <Flex 
@@ -39,31 +60,22 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ theme, title, description, te
         direction='column'
         boxShadow="dark-lg"
         p="10px"
-        w={{ base: "300px", md:"345px"}}
         borderRadius="10px"
+        as="a"
+        href={url}
+        target="_blank"
         >
-        <Flex w='100%' mb='18px' >
-          {/* Card Header Layer: Icon + Title + Link */}
-          <Flex w='38px' h='38px' ml={['0px', '9px']} align='center' justify='center' borderRadius='10px' me='12px' bg={iconBox}>
-            <Icon w='24px' h='24px' as={ MdOutlineDoneAll } color={iconColor} />
-          </Flex>
-          <Text fontWeight='600' fontSize='lg' my='auto' color={titleColor} me='auto'>{title}</Text>
-          <Link href={url} target='_blank'>
-          <Button w='38px' h='38px' borderRadius='50%'>
-            <Icon as={MdLink}/>
-          </Button>
-          </Link>
-        </Flex>
-        {/* Image Layer */}
-        <Image src={image} borderRadius='10px' objectFit="cover" h='168px' w='295px' mb='10px'/>
-        {/* Description Layer */}
-        <Text fontWeight='350' color={descriptionColor} mr={['0px', '10px']} ml={['0px', '10px']} textAlign="justify" w='95%'>{description}</Text>
-        {/* Card Footer Layer: Badge + Collaborators(?) */}
-        <Flex mt='auto' justify='space-between' w='100%' align='center' justifyContent='right'>
+          <Flex w='100%' mb='10px' p='5px'>
+            {/* Card Header Layer: Title + Link */}
+            <Text fontWeight='600' fontSize='lg' my='auto' color={titleColor} me='auto'>{title}</Text>
             {techStack.map((logo, idx) => (
-              <Icon key={idx} as={logo} color="gray.600" boxSize='1.3em'/>
-            ))}
-        </Flex>
+                <IconContainer key={idx} logo={logo}/>
+              ))}
+          </Flex>
+          {/* Image Layer */}
+          <Image src={image} borderRadius='10px' objectFit="cover" h='168px' w='295px' mb='10px'/>
+          {/* Description Layer */}
+          <Text as='a' fontWeight='350' noOfLines={6} color={descriptionColor} textAlign="justify">{description}</Text>
         </Flex>
       </Flex>
     );
